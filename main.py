6440b07cc1e844bdb1e22a300b8b9780fff35490
Andrew Cukierwar <acukierwar@wustl.edu>
2020-08-09 12:19:33 -0400

    Added 1 min and OT alerts, first game alert starting, and returning after last game
    - Now includes 1 Min and OT alerts
    - Checks for first game to start before entering alert loop
    - Stops after last game finishes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime as dt
 from time import time, sleep
 from slack import WebClient
 import requests
@@ -11,27 +12,42 @@ def main():
     # initialize nba
 
     nba_games = nba_alerts.set_games()
-    nba_alerted = set()
+    nba_alerted = {'5 Min': set(), '1 Min': set(), 'OT': set()}
 
     nba_daily_report = nba_alerts.get_daily_report(nba_games)
     response = client.chat_postMessage(channel='#crunchtime-alerts', text=nba_daily_report)
     print('NBA Daily Report Sent')
 
+    lower_window = nba_alerts.get_time_windows(nba_games)
+
     # initialize mlb
 
-    # mlb_games = mlb_alerts.set_games()
-    # mlb_alerted = set()
+    ## mlb_games = mlb_alerts.set_games()
+    ## mlb_alerted = set()
 
-    # mlb_daily_report = mlb_alerts.get_daily_report(mlb_games)
-    # response = client.chat_postMessage(channel='#crunchtime-alerts', text=mlb_daily_report)
-    # print('MLB Daily Report Sent')
+    ## mlb_daily_report = mlb_alerts.get_daily_report(mlb_games)
+    ## response = client.chat_postMessage(channel='#crunchtime-alerts', text=mlb_daily_report)
+    ## print('MLB Daily Report Sent')
 
     # listen for new alerts
 
-    while True:
+    while dt.now() < lower_window: # check every 30 minutes
+        print('Checking to see if first game started')
+        interval = 30 * 60 # 30 minute intervals
+        sleep(interval - time() % interval)
+
+    print('First game started')
+
+    nba_games, nba_alerted = nba_alerts.send_alerts(client, nba_games, nba_alerted)
+
+    while not nba_alerts.is_completed(nba_games):
         nba_games, nba_alerted = nba_alerts.send_alerts(client, nba_games, nba_alerted)
-        # mlb_games, mlb_alerted = mlb_alerts.send_alerts(client, mlb_games, mlb_alerted)
+        ## mlb_games, mlb_alerted = mlb_alerts.send_alerts(client, mlb_games, mlb_alerted)
         sleep(60 - time() % 60)
+
+    print('All games done')
+
+    return
 
 if __name__ == "__main__":
  	main()
--- nba_alerts.py
+++ nba_alerts.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime as dt
 from bs4 import BeautifulSoup
 from slack import WebClient
 import nba_watchability
@@ -38,12 +39,15 @@ def update_games(games):
     return games
 
 def check_for_new_alerts(games, alerted):
-    new_alerts = set()
+    new_alerts = {'5 Min': set(), '1 Min': set(), 'OT': set()}
     for teams, game in games.items():
-        if teams not in alerted and game['quarter'] == 4 and game['clock'] > 0 and game['clock'] <= 300:
-            differential = abs(game['score'][0] - game['score'][1])
-            if differential <= 5:
-                new_alerts.add(teams)
+        differential = abs(game['score'][0] - game['score'][1])
+        if teams not in alerted['5 Min'] and game['quarter'] == 4 and game['clock'] <= 300 and differential <= 5:
+            new_alerts['5 Min'].add(teams)
+        if teams not in alerted['1 Min'] and game['quarter'] == 4 and game['clock'] <= 60 and differential <= 3:
+            new_alerts['1 Min'].add(teams)
+        if teams not in alerted['OT'] and game['quarter'] == 5: # if game['quarter'] >= 5 and game['clock'] == 300
+            new_alerts['OT'].add(teams)
     return new_alerts
 
 def get_daily_report(games):
@@ -52,8 +56,9 @@ def get_daily_report(games):
     for game in games:
         games[game].update(watchability_dict[game])
     all_text = []
-    for teams, game in games.items():
-        text = '<%s|%s @ %s at %s>\n%s Watchability' % (game['url'], teams[0], teams[1], game['time'], game['Watchability'])
+    for (away_team, home_team), game in games.items():
+        text = '<%s|%s @ %s at %s>\n%s Watchability' % (game['url'], away_team, home_team, 
+            game['time'], game['Watchability'])
         all_text.append(text)
     daily_report = '\n\n'.join(all_text)
     return daily_report
@@ -61,11 +66,41 @@ def get_daily_report(games):
 def send_alerts(client, games, alerted):
     games = update_games(games)
     new_alerts = check_for_new_alerts(games, alerted)
-    for teams in new_alerts:
-        game = games[teams]
-        text = '<%s|%s %s, %s %s. %s remaining>' % (game['url'], teams[0], game['score'][0], 
-        	teams[1], game['score'][1], game['displayClock'])
-        response = client.chat_postMessage(channel='#crunchtime-alerts', text=text)
-        print('Alert Sent')
-        alerted.add(teams)
+    for timeframe, all_teams in new_alerts.items():
+        for teams in all_teams:
+            game = games[teams]
+            away_team, home_team = teams
+            away_score, home_score = game['score']
+            text = '<%s|%s %s, %s %s. %s remaining>' % (game['url'], away_team, away_score, 
+            	home_team, home_score, game['displayClock'])
+            response = client.chat_postMessage(channel='#bot-testing', text=text) ## CHANGE BACK
+            print('Alert Sent')
+            alerted[timeframe].add(teams)
     return games, alerted
+
+def get_time_windows(games):
+    today = dt.now().date()
+    first_game_time = list(games.values())[0]['time'].split()[0]
+    lower_window = dt.combine(today, dt.strptime(first_game_time, '%I:%M%p').time())
+    return lower_window
+
+def is_completed(games):
+    for game in games.values():
+        if game['quarter'] < 4 or game['clock'] != 0 or game['score'][0] == game['score'][1]:
+            return False
+    return True
+
+def get_score_report(games):
+    all_text = []
+    for (away_team, home_team), game in games.items():
+        away_score, home_score = game['score']
+        text = '<%s|%s %s, %s %s with %s remaining in Q%s>' % (game['url'], away_team, 
+            away_score, home_team, home_score, game['displayClock'], game['quarter'])
+        if away_score + home_score == 0: # game hasn't started
+            text = '<%s|%s @ %s at %s>\n%s Watchability' % (game['url'], away_team, home_team, 
+                game['time'], game['Watchability'])
+        elif game['quarter'] >= 4 and game['clock'] == 0 and away_score != home_score: # game completed
+            text = '%s %s, %s %s' % (away_team, away_score, home_team, home_score)
+        all_text.append(text)
+    score_report = '\n\n'.join(all_text)
+    return score_report
